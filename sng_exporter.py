c1969a024dfcd2dbe8e4cc539001a7cece17f927
Imre Kalmar <imre.kalmar@balabit.com>
2024-04-23 14:26:59 +0200

    sng_exporter.py: add argument parser

--- sng_exporter.py
+++ sng_exporter.py
@@ -1,16 +1,17 @@
+import argparse
+import logging
 import socket
 import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# TODO socket path, server port, 'STATS PROMETHEUS WITH_LEGACY\n' paraméterezhetően
-
 
 class PrometheusRequestHandler(BaseHTTPRequestHandler):
+    socket_path = None
+    stats_with_legacy = None
     data = None
 
     def handle(self):
-        # print("Run before request")
-        self.data = fetch_syslog_stats()
+        self.data = self.fetch_syslog_stats(self.socket_path, self.stats_with_legacy)
         BaseHTTPRequestHandler.handle(self)
 
     def do_GET(self):
@@ -20,46 +21,66 @@ class PrometheusRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(self.data)
 
+    def fetch_syslog_stats(self, socket_path, stats_with_legacy):
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        print("Fetching syslog-ng stats")
+        try:
+            sock.connect(socket_path)
+        except socket.error as e:
+            logging.error(f"Socket connection error: {e}")
+            sys.exit(1)
+        try:
+            message = stats_with_legacy
+            sock.send(message.encode())
 
-def run_server():
-    server_address = ('localhost', 8000)
-    httpd = HTTPServer(server_address, PrometheusRequestHandler)
+            response = b''
+            while True:
+                chunk = sock.recv(1024)
+                response += chunk
+                if not chunk or b'.\n' in chunk:
+                    break
+
+            print("Received:", response.decode())
+            return response.decode().strip()[:-1].encode()
+
+        finally:
+            print('closing socket')
+            sock.close()
+
+
+class HttpServer:
+    def __init__(self, listen_address, socket_path, stats_type):
+        PrometheusRequestHandler.socket_path = socket_path
+        PrometheusRequestHandler.stats_with_legacy = stats_type
+        server = HTTPServer((listen_address.split(":")[0], int(listen_address.split(":")[1])), PrometheusRequestHandler)
+        server.serve_forever()
+
+
+def run_server(port: int, socket_path, stats_type):
+    server_address = ('localhost', port)
+    httpd = HTTPServer(server_address, PrometheusRequestHandler())
     print('Starting server...')
     httpd.serve_forever()
 
 
-def fetch_syslog_stats():
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    socket_path = '/var/lib/syslog-ng/syslog-ng.ctl'
-    print("Fetching syslog-ng stats")
-    try:
-        sock.connect(socket_path)
-        # print(f"Successfully connected to {socket}")
-    except socket.error:
-        print(socket.error)
-        sys.exit(1)
-    try:
-        message = 'STATS PROMETHEUS WITH_LEGACY\n'
-        sock.send(message.encode())
-
-        response = b''
-        while True:
-            chunk = sock.recv(1024)
-            # breakpoint()
-            response += chunk
-            if not chunk or b'.\n' in chunk:
-                break
-
-        print("Received:", response.decode())
-        return response.decode().strip()[:-1].encode()
-
-    finally:
-        print('closing socket')
-        sock.close()
-
-
 def main():
-    run_server()
+    parser = argparse.ArgumentParser(
+        description="Command line script to export syslog-ng stats to Prometheus.")
+    parser.add_argument("--listen-address", dest="listen_address", help="Listen address", default=':9577')
+    parser.add_argument("--socket-path", dest="socket_path", help="Path to the syslog-ng-ctl socket", default='/var/lib/syslog-ng/syslog-ng.ctl')
+    parser.add_argument("--stats-with-legacy", dest="stats_with_legacy", action="store_true",
+                        help="Enable stats with legacy")
+    args = parser.parse_args()
+
+    stats = 'STATS PROMETHEUS\n'
+    if args.stats_with_legacy:
+        stats = 'STATS PROMETHEUS WITH_LEGACY\n'
+
+    print("Socket Path:", args.socket_path)
+    print("Listen address:", args.listen_address)
+    print("Stats with Legacy:", stats)
+
+    server = HttpServer(args.listen_address, args.socket_path, stats)
 
 
 if __name__ == "__main__":
