89927a4630299bb0f1e598d5da197123ede8f0dd
Imre Kalmar <imre.kalmar@balabit.com>
2024-04-23 17:15:04 +0200

    sng_exporter.py: fix logging and check syslog-ng socket at startup

--- sng_exporter.py
+++ sng_exporter.py
@@ -4,6 +4,7 @@ import socket
 import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
 class PrometheusRequestHandler(BaseHTTPRequestHandler):
     socket_path = None
     stats_with_legacy = None
@@ -21,13 +22,8 @@ class PrometheusRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(self.data)
 
     def fetch_syslog_stats(self, socket_path, stats_with_legacy):
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         logging.info("Fetching syslog-ng stats")
-        try:
-            sock.connect(socket_path)
-        except socket.error as e:
-            logging.error(f"Socket connection error: {e}")
-            sys.exit(1)
+        sock = self.create_socket_connection(socket_path)
         try:
             message = stats_with_legacy
             sock.send(message.encode())
@@ -39,31 +35,48 @@ class PrometheusRequestHandler(BaseHTTPRequestHandler):
                 if not chunk or b'.\n' in chunk:
                     break
 
-            logging.info("Received:", response.decode())
+            logging.info(f"Received: {response.decode()}")
             return response.decode().strip()[:-1].encode()
 
         finally:
             logging.info('closing socket')
             sock.close()
 
+    @classmethod
+    def create_socket_connection(cls, socket_path, check_only=False):
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            sock.connect(socket_path)
+        except socket.error as e:
+            logging.error(f"Socket connection error: {e}: {socket_path}")
+            sys.exit(1)
+
+        if check_only:
+            sock.close()
+            return
+
+        return sock
+
 
 class HttpServer:
     def __init__(self, listen_address, socket_path, stats_type):
         PrometheusRequestHandler.socket_path = socket_path
         PrometheusRequestHandler.stats_with_legacy = stats_type
-        self.server = HTTPServer((listen_address.split(":")[0], int(listen_address.split(":")[1])), PrometheusRequestHandler)
+        self.server = HTTPServer((listen_address.split(":")[0], int(listen_address.split(":")[1])),
+                                 PrometheusRequestHandler)
         try:
             self.server.serve_forever()
         except KeyboardInterrupt:
             print('Server stopped.')
-            self.server.close()
+            self.server.server_close()
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Command line script to export syslog-ng stats to Prometheus.")
     parser.add_argument("--listen-address", dest="listen_address", help="Listen address", default=':9577')
-    parser.add_argument("--socket-path", dest="socket_path", help="Path to the syslog-ng-ctl socket", default='/var/lib/syslog-ng/syslog-ng.ctl')
+    parser.add_argument("--socket-path", dest="socket_path", help="Path to the syslog-ng-ctl socket",
+                        default='/var/lib/syslog-ng/syslog-ng.ctl')
     parser.add_argument("--stats-with-legacy", dest="stats_with_legacy", action="store_true",
                         help="Enable stats with legacy")
     args = parser.parse_args()
@@ -72,9 +85,12 @@ def main():
     if args.stats_with_legacy:
         stats = 'STATS PROMETHEUS WITH_LEGACY\n'
 
-    logging.info("Socket Path:", args.socket_path)
-    logging.info("Listen address:", args.listen_address)
-    logging.info("Stats with Legacy:", stats)
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"Socket Path: {args.socket_path}")
+    logging.info(f"Listen address: {args.listen_address}")
+    logging.info(f"Stats with Legacy: {stats}")
+
+    PrometheusRequestHandler.create_socket_connection(args.socket_path, check_only=True)
     server = HttpServer(args.listen_address, args.socket_path, stats)
 
 
