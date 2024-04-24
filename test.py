8edb05419a8f3a4d22724ba7b1d54a70410a057d
Kristof Mako <kristof.mako@quest.com>
2024-04-24 11:23:20 +0200

    test.py: modified tests
    test request status and content
    test client connect

--- test.py
+++ test.py
@@ -3,24 +3,40 @@ from unittest.mock import patch
 from io import BytesIO
 from http.client import HTTPConnection
 from sng_exporter import PrometheusRequestHandler, HttpServer, main
-import threading
+import multiprocessing
 import socket
 from time import sleep
+import requests
 
 class TestPrometheusServer(unittest.TestCase):
 
-    def test_command_line_arguments(self):
-        args = ['--listen-address', '127.0.0.1:9577', '--socket-path', '/var/lib/syslog-ng/syslog-ng.ctl', '--stats-with-legacy']
+    def test_request(self):
+        args = ['--listen-address', 'localhost:9577', '--socket-path', '/var/lib/syslog-ng/syslog-ng.ctl', '--stats-with-legacy']
         with patch('sys.argv', ['sng_exporter.py'] + args):
-            server_thread = threading.Thread(target=main)
-            server_thread.start()
+            server_proc = multiprocessing.Process(target=main, args=())
+            server_proc.start()
+            # Terminate the process
             sleep(1)
+
+            r = requests.get('http://localhost:9577/metrics')
+            self.assertEqual(r.status_code, 200)
+            self.assertNotEqual(r.content[-1], '.')
+            server_proc.terminate()  # sends a SIGTERMimport socket
+
+    def test_client(self):
+        args = ['--listen-address', 'localhost:9577', '--socket-path', '/var/lib/syslog-ng/syslog-ng.ctl', '--stats-with-legacy']
+        with patch('sys.argv', ['sng_exporter.py'] + args):
+            server_proc = multiprocessing.Process(target=main, args=())
+            server_proc.start()
+            # Terminate the process
+            sleep(1)
+
             fake_client = socket.socket()
             fake_client.settimeout(1)
-            fake_client.connect(('127.0.0.1', 9577))
+            fake_client.connect(('localhost', 9577))
             fake_client.close()
-            print('we are here')
-            server_thread.join()
+            print(fake_client)
+            server_proc.terminate()  # sends a SIGTERMimport socket
 
 
 
